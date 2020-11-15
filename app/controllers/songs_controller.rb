1434e6204bf2940b858ef62f95a8637263543b7f
Cloudymolecule <baires82@gmail.com>
2020-11-15 14:30:04 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,7 +36,7 @@ class SongsController < Sinatra::Base
         # song.genre_ids = params[:genres]
         song.artist = artist
        
-        erb :"/songs/#{song.slug}"
+        redirect to :"/songs/#{song.slug}"
 
     end
 
